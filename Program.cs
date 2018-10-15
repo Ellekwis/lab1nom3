0976eb233fd987a611c0af347c443f6b5b387ad5
Ellekwis <43903852+Ellekwis@users.noreply.github.com>
2018-10-15 18:02:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Артём
+ * Date: 01.10.2018
+ * Time: 14:43
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace lab1nom3
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+		
+			// TODO: Implement Functionality Here
+			
+			Console.WriteLine("Введите максимальное число");
+			int max = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Введите минимальное число");
+			int min = Convert.ToInt32(Console.ReadLine());
+			int [,] A = new int [10,10];
+			Random rnd = new Random();
+			for(int i = 0;i<10;i++)
+			{
+				for(int j=0;j<10;j++)
+				{
+					A[i,j]  = rnd.Next(min,max);
+					Console.Write(A[i,j]+" ");
+					
+				}
+				Console.WriteLine();
+				Console.ReadKey();
+			}
+			}
+			
+	}
+}
